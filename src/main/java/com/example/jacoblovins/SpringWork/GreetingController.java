f41d441fb7e6fdfb29ad457987c29af7ee1e5d48
jacoblovins <lovins.jacob@yahoo.com>
2021-06-14 12:35:27 -0400

    greeting controller working

---
+++ src/main/java/com/example/jacoblovins/SpringWork/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.jacoblovins.SpringWork;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@RequestMapping("/greeting")
+	public String getGreeting() {
+		return "Hello from SpringWork!";
+	}
+}
